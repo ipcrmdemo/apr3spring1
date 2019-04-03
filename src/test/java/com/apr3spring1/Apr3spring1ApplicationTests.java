67816c2238238ae2d8deb465ab73e041e802f9de
Matt Cadorette <mcadorette@gmail.com>
2019-04-03 13:31:00 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/test/java/com/apr3spring1/Apr3spring1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apr3spring1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Apr3spring1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
